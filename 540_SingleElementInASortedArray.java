7ca7a431a2d64712ab50b1cbb78f3eb68932ecd4
Gurukirana G <gurukirankiralu@gmail.com>
2026-03-12 18:22:28 +0530

    Create 540_SingleElementInASortedArray.java

---
+++ 540_SingleElementInASortedArray.java
@@ -0,0 +1,33 @@
+// 540 - Single Element in a Sorted Array
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low=1;
+        int high=nums.length-1;
+        if(nums.length==1){
+            return nums[0];
+        }
+        if(nums[0]!=nums[1]){
+            return nums[0];
+        }
+        if(nums[nums.length-1]!=nums[nums.length-2]){
+            return nums[nums.length-1];
+        }
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1]){
+                return nums[mid];
+            }
+            if(mid%2==1&&nums[mid]==nums[mid-1]||mid%2==0&&nums[mid]==nums[mid+1]){
+                low=mid+1;
+
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return 0;
+    }
+}
