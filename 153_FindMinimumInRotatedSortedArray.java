a69d66d9d3c1ee7013bf53852882a0c583320d06
Gurukirana G <gurukirankiralu@gmail.com>
2026-03-12 15:40:02 +0530

    Implement solution to find minimum in rotated array

---
+++ 153_FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,19 @@
+// 153 - Find Minimum in Rotated Sorted Array
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+class Solution {
+    public int findMin(int[] nums) {
+        int low=0;
+        int high=nums.length-1;
+       while(low<high){
+        int mid=low+(high-low)/2;
+        if(nums[mid]>nums[high]){
+            low=mid+1;
+        }
+        else{
+            high=mid;
+        }
+       }
+       return nums[low];
+    }
+}
