965561dc8864bf7795bbee11c1bcf745eece8929
Gurukirana G <gurukirankiralu@gmail.com>
2026-03-24 23:53:43 +0530

    Add reverseString method to reverse a character array
    Implement a method to reverse a character array in place.

---
+++ 344_ReverseString.java
@@ -0,0 +1,14 @@
+// 344 - Reverse String
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+    public void reverseString(char[] s) {
+      int left=0;
+      int right=s.length-1;
+      while(left<right){
+        char temp=s[left];
+        s[left++]=s[right];
+        s[right--]=temp;
+      }
+    }
+}
