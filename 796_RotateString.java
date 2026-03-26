2093ee489b2806aec5b01bdb45dd0dff28bee060
Gurukirana G <gurukirankiralu@gmail.com>
2026-03-26 23:35:41 +0530

    Create 796_RotateString.java

---
+++ 796_RotateString.java
@@ -0,0 +1,21 @@
+// 796 - Rotate String
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.equals(goal)){
+            return true;
+        }
+        if (s.length() != goal.length()) return false;
+        StringBuilder sb=new StringBuilder(s);
+        for(int i=0;i<s.length();i++){
+            sb.append(sb.charAt(0));
+            sb.deleteCharAt(0);
+            if(sb.toString().equals(goal)){
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
