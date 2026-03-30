6d5de280555e2c82aea006ace63e52569a34cc22
Gurukirana G <gurukirankiralu@gmail.com>
2026-03-30 20:54:47 +0530

    Create 1004_MaxConsecutiveOnesIII.java

---
+++ 1004_MaxConsecutiveOnesIII.java
@@ -0,0 +1,26 @@
+// 1004 - Max Consecutive Ones III
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int max=0;
+        int left=0;
+        int right=0;
+        int zero=0;
+        while(right<nums.length){
+            if(nums[right]==0){
+                zero++;
+            }
+        if(zero>k){
+            if(nums[left]==0){
+                zero--;
+            }
+            left++;
+        }
+          max=Math.max(max,right-left+1);
+            right++;
+
+        }
+        return max;
+    }
+}
