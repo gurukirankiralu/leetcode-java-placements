4b9c8b93405d21906911b2115a228842ea7e9ea9
Gurukirana G <gurukirankiralu@gmail.com>
2026-04-06 18:38:09 +0530

    Add solution for climbing stairs problem
    Implement climbing stairs solution with O(n) time complexity.

---
+++ 70_ClimbingStairs.java
@@ -0,0 +1,19 @@
+// 70 - Climbing Stairs
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+    public int climbStairs(int n) {
+         if (n <= 2) return n;
+
+        int prev2 = 1; 
+        int prev1 = 2; 
+
+        for (int i = 3; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
