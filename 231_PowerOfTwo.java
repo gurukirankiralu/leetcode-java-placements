eb5fc1dffcf2e928a1dc031613078312888c5150
Gurukirana G <gurukirankiralu@gmail.com>
2026-04-10 19:04:20 +0530

    Implement isPowerOfTwo method in Solution class

---
+++ 231_PowerOfTwo.java
@@ -0,0 +1,8 @@
+// 231 - Power of Two
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+          return n > 0 && (n & (n - 1)) == 0;
+    }
+}
